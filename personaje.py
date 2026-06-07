87814bf998a9f8945b2ef1808bf9026c6a0585d9
SantiRautenberg <snrautenberg@unsam-bue.edu.ar>
2026-06-07 19:33:30 -0300

    Resolver conflicto en personaje

--- personaje.py
+++ personaje.py
@@ -14,18 +14,27 @@ class Jugador:
         self.x = x
         self.y = y
 
-        # Escalado uniforme para todos los sprites del personaje
         self.dimensiones = (75, 75)
 
-        # Ruta
-        ruta_carpeta = os.path.dirname(__file__) + "/imagenes/jugador" # Obtiene la ruta del directorio actual
+        ruta_carpeta = os.path.join(os.path.dirname(__file__), "imagenes", "jugador")
 
-        # Los sprites para cada direccion se guardan por separado
         self.sprites_direcciones = {
-            "ABAJO":     pygame.transform.scale(pygame.image.load(os.path.join(ruta_carpeta, "abajo.png")).convert_alpha(), self.dimensiones),
-            "ARRIBA":    pygame.transform.scale(pygame.image.load(os.path.join(ruta_carpeta, "atras.png")).convert_alpha(), self.dimensiones),
-            "IZQUIERDA": pygame.transform.scale(pygame.image.load(os.path.join(ruta_carpeta, "izquierda.png")).convert_alpha(), self.dimensiones),
-            "DERECHA":   pygame.transform.scale(pygame.image.load(os.path.join(ruta_carpeta, "derecha.png")).convert_alpha(), self.dimensiones)
+            "ABAJO": pygame.transform.scale(
+                pygame.image.load(os.path.join(ruta_carpeta, "abajo.png")).convert_alpha(),
+                self.dimensiones
+            ),
+            "ARRIBA": pygame.transform.scale(
+                pygame.image.load(os.path.join(ruta_carpeta, "atras.png")).convert_alpha(),
+                self.dimensiones
+            ),
+            "IZQUIERDA": pygame.transform.scale(
+                pygame.image.load(os.path.join(ruta_carpeta, "izquierda.png")).convert_alpha(),
+                self.dimensiones
+            ),
+            "DERECHA": pygame.transform.scale(
+                pygame.image.load(os.path.join(ruta_carpeta, "derecha.png")).convert_alpha(),
+                self.dimensiones
+            )
         }
 
         self.animacion_caminando_derecha = []
@@ -38,64 +47,60 @@ class Jugador:
 
             imagen = pygame.transform.scale(imagen, self.dimensiones)
 
-            # Si tus caminar_0, caminar_1, etc. miran hacia la IZQUIERDA:
             self.animacion_caminando_izquierda.append(imagen)
-            self.animacion_caminando_derecha.append(pygame.transform.flip(imagen, True, False))
-#----------------------------------------------------------------------------------------------------
-        # --- Variables de control para los estados y el tiempo ---
+            self.animacion_caminando_derecha.append(
+                pygame.transform.flip(imagen, True, False)
+            )
+
         self.direccion_actual = "ABAJO"
         self.esta_moviendose = False
         self.indice_animacion = 0
         self.tiempo_ultimo_frame = 0
-        self.velocidad_animacion = 130  # ms de cada paso
+        self.velocidad_animacion = 130
 
-        # sprite por defecto (Sprite de mirar hacia abajo)
         self.sprite = self.sprites_direcciones[self.direccion_actual]
 
     def ActualizarAnimacion(self):
         tiempo_actual = pygame.time.get_ticks()
 
         if self.esta_moviendose:
+            if self.direccion_actual == "IZQUIERDA":
+                animacion = self.animacion_caminando_izquierda
+            elif self.direccion_actual == "DERECHA":
+                animacion = self.animacion_caminando_derecha
+            else:
+                animacion = self.animacion_caminando_derecha
+
             if tiempo_actual - self.tiempo_ultimo_frame > self.velocidad_animacion:
-                self.indice_animacion += 1
-
-                if self.direccion_actual == "IZQUIERDA":
-                    animacion = self.animacion_caminando_izquierda
-                elif self.direccion_actual == "DERECHA":
-                    animacion = self.animacion_caminando_derecha
-                else:
-                    animacion = self.animacion_caminando_derecha
-
-                self.indice_animacion = self.indice_animacion % len(animacion)
-                self.sprite = animacion[self.indice_animacion]
-
+                self.indice_animacion = (self.indice_animacion + 1) % len(animacion)
                 self.tiempo_ultimo_frame = tiempo_actual
+
+            self.sprite = animacion[self.indice_animacion]
         else:
             self.sprite = self.sprites_direcciones[self.direccion_actual]
 
     def Moverse(self, keys):
-        # Reiniciamos el estado a Falso en cada frame. Solo cambia a Verdadero si toca una tecla.
         self.esta_moviendose = False
 
-        if keys[pygame.K_a]:    # Izquierda
+        if keys[pygame.K_a]:
             self.x -= self.vel_movimiento
             self.direccion_actual = "IZQUIERDA"
             self.esta_moviendose = True
-        elif keys[pygame.K_d]:  # Derecha
+        elif keys[pygame.K_d]:
             self.x += self.vel_movimiento
             self.direccion_actual = "DERECHA"
             self.esta_moviendose = True
-            
-        if keys[pygame.K_w]:    # Arriba
+
+        if keys[pygame.K_w]:
             self.y -= self.vel_movimiento
             self.direccion_actual = "ARRIBA"
             self.esta_moviendose = True
-        elif keys[pygame.K_s]:  # Abajo
+        elif keys[pygame.K_s]:
             self.y += self.vel_movimiento
             self.direccion_actual = "ABAJO"
             self.esta_moviendose = True
+
         self.ActualizarAnimacion()
-        
+
     def Dibujo(self, pantalla):
-        # Renderiza el sprite para cada frame en la posición actual del jugador
         pantalla.blit(self.sprite, (self.x, self.y))
