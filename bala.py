92ba6adc245ace2db1ccf53bd913041ef64eb428
Natalia Irmici <nat.irmici@gmail.com>
2026-06-18 20:12:32 -0300

    Cambios en bala, personaje, escenas, y añado estadísticas
    Las balas toman daño del personaje, modifiqué las listas y arreglé los métodos del personaje.
    Añadí estadísticas.
    Me faltan subir otras correcciones, no conviene mergear todavía.

--- bala.py
+++ bala.py
@@ -1,43 +1,29 @@
 from base import Base
+from estadistica import Estadisticas
 import pygame
-import math
 
 class Bala(Base):
 
-    def __init__(self, x, y, dire_x, dire_Y):
+    def __init__(self, x, y, dire_x, dire_Y, daño):
         super().__init__(x, y)
         self.dire_x = dire_x
         self.dire_Y = dire_Y
         self.bala_vel = 10
-        self.daño = 1
-        self.radio_colision = 15
+        self.daño = daño # lo recibe del jugador
+        Estadisticas.sumar_balas_disparadas()
 
     def trayectoria(self):
-     self.x += self.dire_x * self.bala_vel
-     self.y += self.dire_Y * self.bala_vel
-
-    def colision_jugador(self, jugador):
-
-     distancia = math.hypot(
-        self.x - jugador.x,
-        self.y - jugador.y
-     )
-
-     if distancia < self.radio_colision:
-        jugador.recibirDaño(self.daño)
-        return True
-
-     return False
-
+        self.x += self.dire_x * self.bala_vel
+        self.y += self.dire_Y * self.bala_vel
+        
     def dibujar(self, pantalla):
-       verde_base = (80, 200, 120)
-       verde_oscuro = (40, 120, 70)
+        verde_base = (80, 200, 120)
+        verde_oscuro = (40, 120, 70)
+        
+        pygame.draw.circle(pantalla, verde_base, (self.x, self.y), 10)
+        pygame.draw.circle(pantalla, verde_oscuro, (self.x - 2, self.y - 2), 3)
+        pygame.draw.circle(pantalla, (200, 255, 200), (self.x + 2, self.y + 2), 2)
 
-       pygame.draw.circle( pantalla, verde_base,(int(self.x), int(self.y)),10)
-
-       pygame.draw.circle(pantalla,verde_oscuro,(int(self.x - 2), int(self.y - 2)),3)
-
-       pygame.draw.circle(pantalla,(200, 255, 200),(int(self.x + 2), int(self.y + 2)),2)
-
-    def actualizar(self, pantalla, *args):
+    def actualizar(self, pantalla):
         self.trayectoria()
+    
