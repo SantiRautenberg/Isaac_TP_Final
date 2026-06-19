25984c7d781eacc1e1ae57bee314f207ec78ff14
Mineinsane <dmorozovich@yahoo.com>
2026-06-19 13:01:56 -0300

    Actualiza AudioManager y limpia documentación

--- audio.py
+++ audio.py
@@ -23,7 +23,6 @@ class AudioManager:
 
     # =====================[METODOS DE CONTROL]==========================================
     def reproducir_musica(self, nombre_archivo, volumen=0.2, bucle=-1):
-        """Maneja el flujo de la música de fondo (BGM)."""
         archivo_completo = os.path.join(self.ruta_sonidos, nombre_archivo)
         if os.path.exists(archivo_completo):
             pygame.mixer.music.load(archivo_completo)
@@ -33,11 +32,11 @@ class AudioManager:
             print(f"[Aviso Audio] No se encontró la música: {archivo_completo}")
 
     def detener_musica(self):
-        """Frena la música de fondo en transiciones como menús o pantallas."""
+        # Se pausa la musica durante las transiciones y menues 
         pygame.mixer.music.stop()
 
     def reproducir_sfx(self, nombre_sfx):
-        """Reproduce un efecto de sonido instantáneo (SFX) mediante canales libres."""
+        # Reproducir sonidos de efectos fuera de la musica
         sonido = self.sonidos_sfx.get(nombre_sfx)
         if sonido:
             sonido.play()
